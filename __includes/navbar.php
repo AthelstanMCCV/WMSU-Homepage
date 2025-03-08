c29792f58c0a34cd9a94e3fd186d8424d24fbebd
LazyGamerUwU <neaththane@gmail.com>
2025-03-09 05:50:08 +0800

    Wmsu Campuses w/ animattions (placeholder images)

---
+++ __includes/navbar.php
@@ -0,0 +1,129 @@
+<nav>
+    <section class="nav-cont">
+        <div class="WMSU-Logo-cont">
+            <img src="../imgs/WMSU-Logo.png" alt="" class="WMSU-Logo">
+            <div class="logo-text">
+                <div class="nav-logo-line"></div>
+                <div class="nav-title-cont">
+                    <div class="nav-logo-title inter-bold">WESTERN MINDANAO STATE UNIVERSITY</div>
+                    <div class="nav-logo-subtitle montserrat-regular">A Smart Research University by 2040</div>
+                </div>
+            </div>
+        </div>
+        <div class="nav-links">
+            <a class="inter-extralight" id="Home" href="">HOME</a>
+            <a class="inter-extralight" id="About" href="">ABOUT US <img src="../imgs/Expand Arrow.png" alt=""></a>
+            <div id="About-dropdown" class="nav-dropdown">
+                <div class="dropdown-cont">
+                    <div class="dropdown-title">
+                        <div class="dropdown-content">
+                            <h6 class="inter-bold">CORE VALUES</h6>
+                            <p class="inter-light">Mission</p>
+                            <p class="inter-light">Vision</p>
+                            <p class="inter-light">Quality Policy</p>
+                            <p class="inter-light">University Function</p>
+                        </div>
+                        <div class="nav-divider"></div>
+                        <div class="dropdown-content">
+                            <h6 class="inter-bold">INSTITUTIONAL IDENTITY</h6>
+                            <p class="inter-light">Strategic Plan</p>
+                            <p class="inter-light">Transparency Seal</p>
+                        </div>
+                        <div class="nav-divider"></div>
+                        <div class="dropdown-content">
+                            <h6 class="inter-bold">FOUNDATION</h6>
+                            <p class="inter-light">History of WMSU</p>
+                            <p class="inter-light">News Archive</p>
+                            <p class="inter-light">Bids & Awards</p>
+                            <p class="inter-light">Gallery</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <a class="inter-extralight" id="Admissions" href="">ADMISSIONS <img src="../imgs/Expand Arrow.png" alt=""></a>
+            <div id="Admissions-dropdown" class="nav-dropdown">
+                <div class="dropdown-cont">
+                    <div class="dropdown-title">
+                        <div class="dropdown-content">
+                            <h6 class="inter-bold">ADMISSION GUIDE</h6>
+                            <p class="inter-light">New Students</p>
+                            <p class="inter-light">Old Students</p>
+                            <p class="inter-light">Enrollment Procedure</p>
+                        </div>
+                        <div class="nav-divider"></div>
+                        <div class="dropdown-content">
+                            <h6 class="inter-bold">SCHEDULE OF FEES</h6>
+                            <p class="inter-light">Undergraduate / Non-Corporate Programs</p>
+                            <p class="inter-light">Graduate / Corporate Programs</p>
+                            <p class="inter-light">College of Law</p>
+                            <p class="inter-light">College of Medicine</p>
+                            <p class="inter-light">Masters Degree</p>
+                            <p class="inter-light">Doctorate Degree</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <a class="inter-extralight" id="Academic" href="">ACADEMIC <img src="../imgs/Expand Arrow.png" alt=""></a>
+            <div id="Academic-dropdown" class="nav-dropdown-1">
+                <div class="dropdown-cont">
+                    <div class="dropdown-title">
+                        <div class="dropdown-content">
+                        <h6 class="inter-bold">College of Architecture</h6>
+                            <h6 class="inter-light">Bachelor of Science in Architecture</h6>
+                        <h6 class="inter-bold">College of Agriculture</h6>
+                            <h6 class="inter-light">Bachelor of Science in Agriculture</h6>
+                            <h6 class="inter-light">Bachelor of Science in Food Technology</h6>
+                            <h6 class="inter-light">Bachelor of Science in Agribusiness</h6>
+                            <h6 class="inter-light">Bachelor of Science in Agricultural Technology</h6>
+                            <h6 class="inter-light">Bachelor of Science in Agronomy</h6>
+                            <h6 class="inter-light">Bachelor of Science in Food Processing and Management</h6>
+                        <h6 class="inter-bold">College of Asian & Islamic Studies</h6>
+                        <h6 class="inter-bold">College of Computing Studies</h6>
+                            <h6 class="inter-light">Bachelor of Science in Computer Science</h6>
+                            <h6 class="inter-light">Bachelor of Science in Information Technology</h6>
+                            <h6 class="inter-light">Associate in Computer Technology</h6>
+                            <h6 class="inter-light">Master in Information Technology</h6>
+                        </div>
+                        <div class="nav-divider-2"></div>
+                        <div class="dropdown-content">
+                        <h6 class="inter-bold">College of Criminal Justice Education</h6>
+                        <h6 class="inter-bold">College Of Engineering</h6>
+                            <h6 class="inter-light">Bachelor of Agricultural and Biosystems Engineering</h6>
+                            <h6 class="inter-light">Bachelor of Civil Engineering</h6>
+                            <h6 class="inter-light">Bachelor of Computer Engineering</h6>
+                            <h6 class="inter-light">Bachelor of Electrical Engineering</h6>
+                            <h6 class="inter-light">Bachelor of Environmental Engineering</h6>
+                            <h6 class="inter-light">Bachelor of Geodetic Engineering</h6>
+                            <h6 class="inter-light">Bachelor of Science in Industrial Engineering</h6>
+                            <h6 class="inter-light">Bachelor of Science in Mechanical Engineering</h6>
+                            <h6 class="inter-light">Bachelor of Science in Sanitary Engineering</h6>
+                        <h6 class="inter-bold">College Of Forestry and Environmental Studies</h6>
+                            <h6 class="inter-light">Bachelor of Science in Forestry</h6>
+                            <h6 class="inter-light">Bachelor of Science in Agroforestry</h6>
+                            <h6 class="inter-light">Bachelor of Science in Environmental Science</h6>
+                        </div>
+                        <div class="nav-divider-3"></div>
+                        <div class="dropdown-content">
+                        <h6 class="inter-bold">College of Home Economics</h6>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <a class="inter-extralight" id="Research" href="">RESEARCH <img src="../imgs/Expand Arrow.png" alt=""></a>
+            <div id="Research-dropdown" class="nav-dropdown">
+                <h6 class="dropdown-title">RESEARCH</h6>
+            </div>
+            <a class="inter-extralight" id="Other-Links" href="">OTHER LINKS <img src="../imgs/Expand Arrow.png" alt=""></a>
+            <div id="Other-dropdown" class="nav-dropdown">
+                    <div class="dropdown-title">
+                        <div class="dropdown-content">
+                            <a href="../page/linkages.php">Linkages</a>
+                        </div>
+                    </div>
+            </div>
+            <div class="MyWmsu-btn">
+                <a class="MyWmsu-link inter-regular" href="">MyWMSU</a>
+            </div>
+        </div>
+    </section>
+</nav>
