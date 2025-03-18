b40d52cffb55ed621555286d016ac6020ab28c68
AthelstanMCCV <165026195+AthelstanMCCV@users.noreply.github.com>
2025-03-18 18:22:30 +0800

    Update homepage-views.php

--- page-views/homepage-views.php
+++ page-views/homepage-views.php
@@ -266,6 +266,34 @@
 
 <section class="line-page-div"></section>
 
-
+<section class="follow-wmsu">
+    <div class="main-page-titles">FOLLOW WMSU</div>
+    <div class="follow-cont">
+        <div class="follow-cont-left">
+            <div class="follow-left-rect">
+                <div class="left-red-overlay"></div>
+                <img src="../imgs/facebook.jpg" alt="" class="follow-left-pic">
+                <div class="follow-left-text">
+                    <div class="left-text-icon">
+                        <img src="../imgs/facebook-icon.png" alt="" >
+                    </div>
+                    <div class="left-text-word">FACEBOOK</div>
+                    </div>
+                </div>
+            </div>
+        <div class="follow-cont-right">
+            <div class="follow-right-rect">
+                <div class="right-red-overlay"></div>
+                <img src="../imgs/youtube.jpg" alt="" class="follow-right-pic">
+                <div class="follow-right-text">
+                    <div class="right-text-word">YOUTUBE</div>
+                    <div class="right-text-icon">
+                        <img src="../imgs/youtube-icon.png" alt="">
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
 
 
