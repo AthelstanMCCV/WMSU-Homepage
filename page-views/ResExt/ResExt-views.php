11cfb82a26933a4f2730f81bee2a47c50bde090c
AthelstanMCCV <165026195+AthelstanMCCV@users.noreply.github.com>
2025-03-22 23:27:28 +0800

    ResExt Updates

--- page-views/ResExt/ResExt-views.php
+++ page-views/ResExt/ResExt-views.php
@@ -59,5 +59,117 @@
 </section>
 
 <section class="ResExtMisVisCont">
+    <div class="ResExtMisVisTop">
+        <p class="ResExtMisVisTitle inter-black">Mission & Vision</p>
+        <div class="ResExtMisVisTitleLine"></div>
+    </div>
+    <div class="ResExtMisVisBoxCont">
+        <div class="ResExtMisVisBox">
+            <p class="ResExtMisVisBoxText inter-regular">The Research & Development Cluster aims to be the vital enabler of research and development  services in WMSU for faculty and students to create socially responsive innovative research and  scientific breakthroughs that blend cutting-edge technologies with human-centric approaches,  creating a harmonious and sustainable quality of life for individuals and industries.</p>
+            <a href="" class="ResExtMisVisBoxBtn">Mission</a>
+        </div>
+        <div class="ResExtMisVisBox">
+            <p class="ResExtMisVisBoxText inter-regular">The Research & Development Cluster aims to be the vital enabler of research and development  services in WMSU for faculty and students to create socially responsive innovative research and  scientific breakthroughs that blend cutting-edge technologies with human-centric approaches,  creating a harmonious and sustainable quality of life for individuals and industries.</p>
+            <a href="" class="ResExtMisVisBoxBtn">Vision</a>
+        </div>
+    </div>
+</section>
 
+<section class="ResExtCoreCompCont">
+    <div class="ResExtCoreComp">
+        <div class="ResExtCoreCompTitleCont">
+            <p class="ResExtCoreCompTitle inter-black">Core Competencies</p>
+            <img class="ResExtCoreCompArrow" src="../imgs/Arrow 2.png" alt="">
+        </div>
+        <div class="ResExtCoreCompScrollable" id="ResExtCoreCompScrollable">
+            <div class="ResExtCoreCompScrollElem"><p class="ResExtCoreCompScrollElemText inter-medium">Research Collaboration and Linkages</p></div>
+            <div class="ResExtCoreCompScrollElem"><p class="ResExtCoreCompScrollElemText inter-medium">Technology Generation and Innovation</p></div>
+            <div class="ResExtCoreCompScrollElem"><p class="ResExtCoreCompScrollElemText inter-medium">Evaluation and Analysis</p></div>
+            <div class="ResExtCoreCompScrollElem"><p class="ResExtCoreCompScrollElemText inter-medium">Policy Formulation</p></div>
+            <div class="ResExtCoreCompScrollElem"><p class="ResExtCoreCompScrollElemText inter-medium">Planning and Development of Groundbreaking Knowledge</p></div>
+            <div class="ResExtCoreCompScrollElem"><p class="ResExtCoreCompScrollElemText inter-medium">Institution Building and Capacity Development</p></div>
+            <div class="ResExtCoreCompScrollElem"><p class="ResExtCoreCompScrollElemText inter-medium">Promotion and Dissemination</p></div>
+        </div>
+    </div>
+</section>
+
+<section class="ResExtGoalsCont">
+    <div class="ResExtGoals">
+        <div class="ResExtGoalsTitleCont">
+            <p class="ResExtGoalsTitle inter-black">Goals</p>
+            <div class="ResExtGoalsTitleLine"></div>
+        </div>
+        <div class="ResExtGoalsContent">
+            <div class="ResExtGoalsImg"></div>
+            <div class="ResExtGoalsInfo">
+                <div class="ResExtGoalsTextCont">
+                    <p class="ResExtGoalsNumTitle inter-black">3.4.1</p>
+                    <p class="ResExtGoalsSubText inter-medium">Active Partnerships with industries and International Network </p>
+                </div>
+                <div class="ResExtGoalsTextCont">
+                    <p class="ResExtGoalsNumTitle inter-black">3.4.1</p>
+                    <p class="ResExtGoalsSubText inter-medium">Active Partnerships with industries and International Network </p>
+                </div>
+                <div class="ResExtGoalsTextCont">
+                    <p class="ResExtGoalsNumTitle inter-black">3.4.1</p>
+                    <p class="ResExtGoalsSubText inter-medium">Active Partnerships with industries and International Network </p>
+                </div>
+                <div class="ResExtGoalsTextCont">
+                    <p class="ResExtGoalsNumTitle inter-black">3.4.1</p>
+                    <p class="ResExtGoalsSubText inter-medium">Active Partnerships with industries and International Network </p>
+                </div>
+                <div class="ResExtGoalsTextCont">
+                    <p class="ResExtGoalsNumTitle inter-black">3.4.1</p>
+                    <p class="ResExtGoalsSubText inter-medium">Active Partnerships with industries and International Network </p>
+                </div>
+                <div class="ResExtGoalsTextCont">
+                    <p class="ResExtGoalsNumTitle inter-black">3.4.1</p>
+                    <p class="ResExtGoalsSubText inter-medium">Active Partnerships with industries and International Network </p>
+                </div>
+                <div class="ResExtGoalsTextCont">
+                    <p class="ResExtGoalsNumTitle inter-black">3.4.1</p>
+                    <p class="ResExtGoalsSubText inter-medium">Active Partnerships with industries and International Network </p>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+<section class="ResExtObjectivesCont">
+    <div class="ResExtObjectives">
+        <div class="ResExtObjectivesTitleCont">
+            <p class="ResExtObjectivesTitle inter-black">Objectives</p>
+            <img class="ResExtObjectivesArrow" src="../imgs/Arrow 1.png" alt="">
+        </div>
+        <div class="ResExtObjectivesContent">
+            <div class="ResExtObjectivesTimeline">
+                <div class="ResExtObjectivesTimelineCircle inter-black active">1</div>
+                    <div class="ResExtObjectivesTimelineLine"></div>
+                <div class="ResExtObjectivesTimelineCircle inter-black">2</div>
+                    <div class="ResExtObjectivesTimelineLine"></div>
+                <div class="ResExtObjectivesTimelineCircle inter-black">3</div>
+                    <div class="ResExtObjectivesTimelineLine"></div>
+                <div class="ResExtObjectivesTimelineCircle inter-black">4</div>
+                    <div class="ResExtObjectivesTimelineLine"></div>
+                <div class="ResExtObjectivesTimelineCircle inter-black">5</div>
+                    <div class="ResExtObjectivesTimelineLine"></div>
+                <div class="ResExtObjectivesTimelineCircle inter-black">6</div>
+                    <div class="ResExtObjectivesTimelineLine"></div>
+                <div class="ResExtObjectivesTimelineCircle inter-black">7</div>
+                    <div class="ResExtObjectivesTimelineLine"></div>
+                <div class="ResExtObjectivesTimelineCircle inter-black">8</div>
+                    <div class="ResExtObjectivesTimelineLine"></div>
+            </div>
+            <div class="ResExtObjectivesTimelineText">
+                <p class="ResExtObjectivesTLtext"><span class="inter-black">3.5.1</span> Establish the overall management framework to direct and control all Research and  Development activities of the university and facilitate quality research through the  provision of adequate research facilities and comprehensive support services fostering a  collaborative environment that generate interdisciplinary research and innovation  programs, projects and activities: (RDEC); Research Development and Evaluation Center </p>
+                <p class="ResExtObjectivesTLtext"><span class="inter-black">3.5.1</span> Establish the overall management framework to direct and control all Research and  Development activities of the university and facilitate quality research through the  provision of adequate research facilities and comprehensive support services fostering a  collaborative environment that generate interdisciplinary research and innovation  programs, projects and activities: (RDEC); Research Development and Evaluation Center </p>
+                <p class="ResExtObjectivesTLtext"><span class="inter-black">3.5.1</span> Establish the overall management framework to direct and control all Research and  Development activities of the university and facilitate quality research through the  provision of adequate research facilities and comprehensive support services fostering a  collaborative environment that generate interdisciplinary research and innovation  programs, projects and activities: (RDEC); Research Development and Evaluation Center </p>
+                <p class="ResExtObjectivesTLtext"><span class="inter-black">3.5.1</span> Establish the overall management framework to direct and control all Research and  Development activities of the university and facilitate quality research through the  provision of adequate research facilities and comprehensive support services fostering a  collaborative environment that generate interdisciplinary research and innovation  programs, projects and activities: (RDEC); Research Development and Evaluation Center </p>
+                <p class="ResExtObjectivesTLtext"><span class="inter-black">3.5.1</span> Establish the overall management framework to direct and control all Research and  Development activities of the university and facilitate quality research through the  provision of adequate research facilities and comprehensive support services fostering a  collaborative environment that generate interdisciplinary research and innovation  programs, projects and activities: (RDEC); Research Development and Evaluation Center </p>
+                <p class="ResExtObjectivesTLtext"><span class="inter-black">3.5.1</span> Establish the overall management framework to direct and control all Research and  Development activities of the university and facilitate quality research through the  provision of adequate research facilities and comprehensive support services fostering a  collaborative environment that generate interdisciplinary research and innovation  programs, projects and activities: (RDEC); Research Development and Evaluation Center </p>
+                <p class="ResExtObjectivesTLtext"><span class="inter-black">3.5.1</span> Establish the overall management framework to direct and control all Research and  Development activities of the university and facilitate quality research through the  provision of adequate research facilities and comprehensive support services fostering a  collaborative environment that generate interdisciplinary research and innovation  programs, projects and activities: (RDEC); Research Development and Evaluation Center </p>
+                <p class="ResExtObjectivesTLtext"><span class="inter-black">3.5.1</span> Establish the overall management framework to direct and control all Research and  Development activities of the university and facilitate quality research through the  provision of adequate research facilities and comprehensive support services fostering a  collaborative environment that generate interdisciplinary research and innovation  programs, projects and activities: (RDEC); Research Development and Evaluation Center </p>
+            </div>
+        </div>
+    </div>
 </section>
